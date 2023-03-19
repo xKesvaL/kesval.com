9a352b023d254018ee0e50b2a2f8f30d8c541444
KesvaL <jabeddou@gmail.com>
2023-03-19 20:23:20 +0100

    feat: Added lorem for scrolling test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,3 +3,24 @@
 </script>
 
 <Hero />
+
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae et doloremque perspiciatis architecto provident
+corrupti, aut saepe, ducimus aliquam animi blanditiis natus quidem commodi tempore inventore velit ab, facere quasi.
+Velit maiores rem porro in sint? Ducimus sunt error sed doloremque dolor veritatis, qui earum molestias voluptate,
+voluptatibus ullam sapiente corporis at ut ab autem alias officia, amet perferendis totam! Nulla perferendis, odio
+nostrum suscipit harum animi quos facilis tempore neque pariatur expedita similique debitis placeat sit molestiae a
+itaque ipsam assumenda porro veniam nesciunt sequi saepe doloribus. Unde, veniam? In accusamus quisquam laudantium
+quibusdam quasi nihil dolorem esse quo doloribus ad ea, nesciunt unde. Temporibus suscipit exercitationem quidem
+cupiditate voluptates, necessitatibus id? Eligendi fugiat, omnis magnam dolorem esse expedita. Vero amet praesentium
+corporis soluta, velit a, exercitationem quasi non facilis quod aliquid debitis, maxime possimus reprehenderit molestiae
+consequatur recusandae mollitia labore! Voluptatibus quibusdam ut dolores vitae voluptatum, repudiandae est. Quisquam
+sequi voluptatum dolores aperiam molestias exercitationem ex enim quaerat natus at incidunt porro laborum officia, iste
+suscipit minus nihil deserunt quas amet aut animi. Labore tenetur deserunt facere et. Quibusdam perspiciatis quod
+assumenda nesciunt quisquam commodi unde rem quis aperiam autem. Enim natus esse debitis aperiam voluptatem labore
+maxime dolore minus earum. Quod harum earum tempora accusantium dignissimos id? Error repellat facilis perferendis
+minima corrupti vero molestiae consequuntur quia cumque. Numquam explicabo veniam aliquam cum cupiditate suscipit velit
+recusandae, in neque dicta saepe placeat sed dolores reiciendis accusantium tempore. Consequuntur tempore corporis
+obcaecati fuga! Blanditiis, architecto illum. Ex, quam. Expedita vitae iste natus, quidem voluptatem pariatur facilis
+architecto, autem harum voluptatibus repellat sint qui sed veniam, praesentium soluta saepe. Quis dignissimos corrupti
+quisquam natus ea fuga tempora voluptatibus beatae obcaecati nulla dolor vero, ut, facilis numquam delectus? Harum est
+eos, aut tempora facilis quae quis eum animi sequi consequatur.
