ba55f480beaba4bb599efce43b2700f12af488f7
KesvaL <47763141+xKesvaL@users.noreply.github.com>
2023-03-20 11:37:25 +0100

    feat: Footer name + ThemeSwitcher

--- src/lib/components/organisms/Footer.svelte
+++ src/lib/components/organisms/Footer.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
   import Socials from '../molecules/Socials.svelte';
+  import ThemeSwitcher from '../molecules/ThemeSwitcher.svelte';
 </script>
 
 <footer>
-  <div />
-  <div />
-  <div><Socials color="var(--color-static-text)" /></div>
+  <div><h2>Jordan<br>Abeddou</h2></div>
+  <div><Socials color="var(--color-static-text)" /><ThemeSwitcher /></div>
   <div class="copyright">KesvaL © 2023. All rights reserved</div>
 </footer>
 
@@ -13,18 +13,22 @@
   @use '$lib/scss/mixins' as *;
 
   footer {
-    min-height: 30vh;
+    min-height: 20vh;
     background: hsl(var(--color-primary-hue), var(--color-primary-saturation), 30%);
     color: var(--color-static-text);
     padding: 0 10rem;
     display: grid;
     align-items: center;
     justify-items: center;
-    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-columns: 1fr;
 
     .copyright {
       grid-column: 1/-1;
       text-align: center;
+    }
+
+    @include breakpoint(md) {
+      grid-template-columns: 1fr 1fr;
     }
   }
 </style>
