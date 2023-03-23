686a7fdf7c7b90941483a49f665a21c62a8f41cd
KesvaL <jabeddou@gmail.com>
2023-03-23 15:51:31 +0100

    feat: Vercel Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,20 @@
   import ContactBot from '$lib/components/molecules/ContactBot.svelte';
   import Footer from '$lib/components/organisms/Footer.svelte';
   import Header from '$lib/components/organisms/Header.svelte';
+  import { page } from '$app/stores';
+  import { browser } from '$app/environment';
   import '$lib/scss/main.scss';
+  import { webVitals } from '$lib/utils/vitals';
+
+  let analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
+
+  $: if (browser && analyticsId) {
+    webVitals({
+      path: $page.url.pathname,
+      params: $page.params,
+      analyticsId,
+    });
+  }
 </script>
 
 <Header />
--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [sveltekit()],
+  define: {
+    'import.meta.env.VERCEL_ANALYTICS_ID': JSON.stringify(process.env.VERCEL_ANALYTICS_ID),
+  },
 });
