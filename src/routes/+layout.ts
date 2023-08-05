5099f36ee35375a15bdf6bde43423300f08633b4
xKesvaL <jabeddou@gmail.com>
2023-08-05 19:52:05 +0200

    fix: lang setting

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,22 +1,16 @@
 import { browser } from '$app/environment';
-import { email } from '$lib/utils/data';
+import { email, langCookieParams } from '$lib/utils/data';
 import '$lib/i18n';
-import { locale, waitLocale, locales } from 'svelte-i18n';
+import { locale, waitLocale } from 'svelte-i18n';
 import type { LayoutLoad } from './$types';
 import Cookies from 'js-cookie';
 
 export const load: LayoutLoad = async ({ url, parent }) => {
   if (browser) {
-    let paramsLang = url.searchParams.get('lang');
-    locales.subscribe((l) => {
-      if (!paramsLang || !l.includes(paramsLang)) {
-        paramsLang = null;
-      } else {
-        Cookies.set('lang', paramsLang);
-      }
-    });
-
-    locale.set(paramsLang || Cookies.get('lang') || window.navigator.language);
+    const paramsLang = url.searchParams.get('lang');
+    const lang = paramsLang || Cookies.get('lang') || window.navigator.language || 'en';
+    Cookies.set('lang', lang, langCookieParams);
+    locale.set(lang);
   }
   await waitLocale();
 
