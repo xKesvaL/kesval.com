6fdce6fbfc7388e6a22f6fb2ce64a7511893c440
xKesvaL <jabeddou@gmail.com>
2023-08-05 20:37:58 +0200

    fix: remove console log

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,7 +4,6 @@ import { locale } from 'svelte-i18n';
 export const handle: Handle = async ({ event, resolve }) => {
   let lang = event.url.searchParams.get('lang') || event.request.headers.get('accept-language')?.split(',')[0];
   lang = lang?.split('-')[0];
-  console.log('hooks.server.ts', lang);
   if (lang) {
     locale.set(lang);
   }
