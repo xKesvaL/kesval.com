164552e3819450f01f9b7fe0fc0f5d08b3a84f3c
xKesvaL <jabeddou@gmail.com>
2023-08-05 19:42:26 +0200

    fix: projects listing

--- src/routes/projects/(projects)/+layout.svelte
+++ src/routes/projects/(projects)/+layout.svelte
@@ -1,20 +1,27 @@
 <script lang="ts">
+  import type { Project } from '$lib';
   import Breadcrumbs from '$lib/components/base/Breadcrumbs.svelte';
   import RelatedProjects from '$lib/components/projects/RelatedProjects.svelte';
+  import { json } from 'svelte-i18n';
   import type { LayoutData } from './$types';
 
   export let data: LayoutData;
 
-  const { project, projects } = data;
+  const { url } = data;
+
+  let projects: Project[];
+  let project: Project | undefined;
+  $: projects = $json('projects.list') as Project[];
+  $: project = projects.find((p) => p.href === url);
 </script>
 
 {#if project}
-<section class="container">
-  <Breadcrumbs type="project" href={project.href} name={project.name} />
-  <div class="prose">
-    <slot />
-  </div>
-</section>
+  <section class="container">
+    <Breadcrumbs type="project" href={project.href.split('/').at(-1) || ''} name={project.name} />
+    <div class="prose">
+      <slot />
+    </div>
+  </section>
 {/if}
 
 {#if project}
