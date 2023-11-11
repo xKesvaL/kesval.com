946db1459f4d4fbdaeda3655501061a948ab0ab8
xKesvaL <jabeddou@gmail.com>
2023-11-11 20:45:45 +0100

    feat: home about finished

--- src/lib/containers/home/HomeAbout.svelte
+++ src/lib/containers/home/HomeAbout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import CardGlass from '$lib/components/cards/CardGlass.svelte';
+	import { Button } from '$lib/components/ui/button';
 	import PERSONAL from '$lib/data/personal';
 	import { t } from 'svelte-i18n';
 </script>
@@ -8,19 +9,18 @@
 	<h2 class="text-center text-4xl lg:text-5xl">
 		{$t('hero.about.title')}
 	</h2>
-	<div class="grid gap-8 lg:grid-cols-2 lg:gap-16">
-		<div class="flex h-full flex-col gap-4">
-			<h3 class="mb-2 font-sans text-4xl font-bold">
+	<div class="grid gap-8 lg:grid-cols-2 lg:items-center lg:gap-16">
+		<div class="flex flex-col gap-4">
+			<h3 class="mb-2 hidden font-sans text-4xl font-bold lg:flex">
 				{$t('home.about.passionate')}
 			</h3>
-			<p>
-				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est quisquam pariatur
-				reprehenderit error porro provident dolores laboriosam a. Quaerat, eum consequatur.
-				Perferendis, labore? Eius omnis suscipit aspernatur.
+			<p class="text-lg lg:text-xl">
+				Interested in development since I was a child, I began coding at the age of 8 with the
+				creation of minecraft plugins and mods.
 			</p>
-			<p>
-				Interested in development since I was a child, I started with the creation of minecraft
-				plugins then I moved on to discord bots. As of today, I'm a web developer as you can see!
+			<p class="text-lg lg:text-xl">
+				Then, as discord rose in popularity, I moved on to making discord bots. As of today, I'm a
+				fullstack web developer as you can see!
 			</p>
 		</div>
 		<div class="grid grid-cols-2 grid-rows-2 gap-4">
@@ -50,6 +50,9 @@
 			>
 		</div>
 	</div>
+	<Button class="mx-auto">
+		{$t('home.about.button')}
+	</Button>
 </section>
 
 <style lang="scss">
