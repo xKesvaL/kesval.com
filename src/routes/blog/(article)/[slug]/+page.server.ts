1c957af513285f84cacb589b46badb9184771ff4
xKesvaL <djorden68000@gmail.com>
2024-02-24 11:15:13 +0100

    fix: re-add entries to blog

--- src/routes/blog/(article)/[slug]/+page.server.ts
+++ src/routes/blog/(article)/[slug]/+page.server.ts
@@ -6,7 +6,7 @@ export const load: PageServerLoad = async ({ parent }) => {
 	return data;
 };
 
-// export const entries: EntryGenerator = async () => {
-// 	const slugs = await getAllSlugs();
-// 	return slugs.map((slug) => ({ slug }));
-// };
+export const entries: EntryGenerator = async () => {
+	const slugs = await getAllSlugs();
+	return slugs.map((slug) => ({ slug }));
+};
