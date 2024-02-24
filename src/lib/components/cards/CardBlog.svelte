b6cc16a4ce8abcd4ef175001f3059aab94fa5d8c
xKesvaL <djorden68000@gmail.com>
2024-02-24 13:12:13 +0100

    feat: prepare blog list and blog post card

--- src/lib/components/cards/CardBlog.svelte
+++ src/lib/components/cards/CardBlog.svelte
@@ -4,8 +4,32 @@
 	import CardGlass from './CardGlass.svelte';
 
 	export let post: BlogPost;
+	export let isLazy = false;
+
+	$: console.log(post);
 </script>
 
-<CardGlass href={route('/blog/[slug]', { slug: post.slug })}>
-	{post.title}
+<CardGlass class="@container/card" classBg="p-0" href={route('/blog/[slug]', { slug: post.slug })}>
+	<div class="grid gap-4 @2xl/card:grid-cols-2">
+		{#if post.coverImage}
+			<div class="max-h-[22rem] min-h-64">
+				<enhanced:img
+					src={post.coverImage}
+					alt={post.coverImageAlt}
+					loading={isLazy ? 'lazy' : 'eager'}
+				/>
+			</div>
+		{/if}
+		<div class="flex flex-col p-4">
+			<h3 class="font-title text-lg">
+				{post.title}
+			</h3>
+			<span class="text-sm text-muted-foreground">
+				{post.readingTime}
+			</span>
+			<p>
+				{post.excerpt}
+			</p>
+		</div>
+	</div>
 </CardGlass>
--- src/lib/containers/blog/BlogList.svelte
+++ src/lib/containers/blog/BlogList.svelte
@@ -5,6 +5,42 @@
 	export let posts: PaginatedResponse<BlogPost>;
 </script>
 
-{#each posts.items as post}
-	<CardBlog {post} />
-{/each}
+<section class="container">
+	<div class="grid gap-4 lg:grid-cols-6">
+		{#each posts.items as post, i (post.slug)}
+			<CardBlog {post} isLazy={i > 6} />
+		{/each}
+	</div>
+</section>
+
+<style lang="scss">
+
+
+	.grid {
+		// Select every 6 elements, starting from position 1
+		// And make it take up 6 columns
+		> :global(:nth-child(6n + 1)) {
+			grid-column: span 6;
+		}
+
+		// Select every 6 elements, starting from position 2
+		// And make it take up 3 columns
+		> :global(:nth-child(6n + 2)) {
+			grid-column: span 3;
+		}
+
+		// Select every 6 elements, starting from position 3
+		// And make it take up 3 columns
+		> :global(:nth-child(6n + 3)) {
+			grid-column: span 3;
+		}
+
+		// Select every 6 elements, starting from position 4, 5 and 6
+		// And make it take up 2 columns
+		> :global(:nth-child(6n + 4)),
+		:global(:nth-child(6n + 5)),
+		:global(:nth-child(6n + 6)) {
+			grid-column: span 2;
+		}
+	}
+</style>
