e6a6f16bbd074f32445274349cf17cebc0345d8e
xKesvaL <jabeddou@gmail.com>
2025-03-10 21:21:41 +0100

    fix: make resume a thing so i doesnt crash on build

---
+++ src/lib/containers/about/AboutResume.svelte
@@ -0,0 +1,1 @@
+<section class="kcontainer min-h-screen-wo-nav" id="resume">test</section>
--- src/routes/a-propos/+page.svelte
+++ src/routes/a-propos/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import AboutHero from '$lib/containers/about/AboutHero.svelte';
+	import AboutResume from '$lib/containers/about/AboutResume.svelte';
 </script>
 
 <AboutHero />
+<AboutResume />
