77d4ab23dba9c4e2708a8540d7073a91c93f86d4
xKesvaL <jabeddou@gmail.com>
2025-05-27 11:46:02 +0200

    feat: update shadcn-svelte

--- src/lib/hooks/is-mobile.svelte.ts
+++ src/lib/hooks/is-mobile.svelte.ts
@@ -1,9 +1,9 @@
-import { MediaQuery } from 'svelte/reactivity';
+import { MediaQuery } from "svelte/reactivity";
 
 const MOBILE_BREAKPOINT = 768;
 
 export class IsMobile extends MediaQuery {
-	constructor(bp: number = MOBILE_BREAKPOINT) {
-		super(`max-width: ${bp - 1}px`);
+	constructor() {
+		super(`max-width: ${MOBILE_BREAKPOINT - 1}px`);
 	}
 }
