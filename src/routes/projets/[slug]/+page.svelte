d1173d5d176ad19d254ed85108d8bd89f2dd8e6a
xKesvaL <jabeddou@gmail.com>
2025-06-23 22:07:02 +0200

    fix: make projects page work in progress for future upload to main

--- src/routes/projets/[slug]/+page.svelte
+++ src/routes/projets/[slug]/+page.svelte
@@ -1,1 +1,14 @@
-hi
+<script lang="ts">
+	import { route } from '$lib/ROUTES';
+	import { localizeHref } from '$paraglide/runtime';
+</script>
+
+<section class="section-hero flex-col items-center justify-center gap-4 text-center">
+	<h1>This page is a work in progress!</h1>
+	<p class="text-lg lg:text-xl">
+		Sorry for the inconvenience. Go back to the <a
+			class="text-primary underline"
+			href={localizeHref(route('/projets'))}>projects page</a
+		>
+	</p>
+</section>
