77a4a562f14684f11de458bdd1fa41a4c6ad739a
KesvaL <47763141+xKesvaL@users.noreply.github.com>
2025-07-04 17:24:41 +0200

    fix: google sitemap error

--- src/routes/(assets)/sitemap.xml/+server.ts
+++ src/routes/(assets)/sitemap.xml/+server.ts
@@ -92,8 +92,8 @@ const generateSitemapString = (sitemapEntries: SitemapEntry[]): string => {
 	let sitemapString = `
     <?xml version="1.0" encoding="UTF-8" ?>
     <urlset
-        xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
-        xmlns:xhtml="https://www.w3.org/1999/xhtml"
+        xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:xhtml="http://www.w3.org/1999/xhtml"
         xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
         xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
         xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
