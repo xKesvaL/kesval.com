7243bef682cb6e9a9f68c4090a7d03e626949442
xKesvaL <jabeddou@gmail.com>
2025-07-15 21:11:00 +0200

    fix: add fallback because old site is still indexed...

---
+++ src/routes/[...fallback]/+page.ts
@@ -0,0 +1,16 @@
+import { route } from '$lib/ROUTES.js';
+import { redirect } from '@sveltejs/kit';
+
+export const load = async ({ params }) => {
+  const fallback = params.fallback;
+  const parts = fallback.split('/');
+
+  if (parts[0] === 'fr') {
+    return redirect(302, route('/'));
+  }
+    
+  return {
+    status: 404,
+    message: 'Not found'
+  }
+}
