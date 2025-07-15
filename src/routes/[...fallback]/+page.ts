95e3ff9959454729a7eb7aa54328a8a011342df2
xKesvaL <jabeddou@gmail.com>
2025-07-15 21:18:39 +0200

    fix: dont prerender fallback route

--- src/routes/[...fallback]/+page.ts
+++ src/routes/[...fallback]/+page.ts
@@ -1,16 +1,18 @@
-import { route } from '$lib/ROUTES.js';
-import { redirect } from '@sveltejs/kit';
+import { route } from "$lib/ROUTES.js";
+import { redirect } from "@sveltejs/kit";
 
 export const load = async ({ params }) => {
-  const fallback = params.fallback;
-  const parts = fallback.split('/');
+	const fallback = params.fallback;
+	const parts = fallback.split("/");
 
-  if (parts[0] === 'fr') {
-    return redirect(302, route('/'));
-  }
-    
-  return {
-    status: 404,
-    message: 'Not found'
-  }
-}
+	if (parts[0] === "fr") {
+		return redirect(302, route("/"));
+	}
+
+	return {
+		status: 404,
+		message: "Not found",
+	};
+};
+
+export const prerender = false;
